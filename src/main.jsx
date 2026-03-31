5e6b9720b1a2638ad077adc6840e2f59043b60f5
25pranjali <it23.pranjali.thosar@kccemsr.edu.in>
2026-03-31 17:56:28 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/E-Waste_IQ">
       <App />
     </BrowserRouter>
   </StrictMode>,
